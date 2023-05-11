f0eced7d9460eae73a038f7f9b43fe0f520daa7c
Chase Poirier <chase.poirier@charter.com>
2023-05-11 07:37:05 -0600

    Update scenario

--- src/Page.tsx
+++ src/Page.tsx
@@ -6,8 +6,16 @@ import { Video } from "./interfaces";
 
 /**
  *
- * Scenario:
+ * Scenario
  *
+ * As a user I would like to view a "News All Day" swimlane that lists
+ * video thumbnails with their titles.
+ *
+ * Acceptance Criteria
+ * - Has a header titled "News All Day"
+ * - Calls videos api endpoint to fetch video items
+ * - Renders a list of VideoCards with the data returned from the api
+ * - Layout matches the design provided
  *
  */
 
